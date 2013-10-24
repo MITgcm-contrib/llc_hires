354c937c433cb1dba77f5dcef7d7ff725c20e16e
Dimitris Menemenlis <dmenemenlis@gmail.com>
2013-10-24 22:04:41 +0000

    small cosmetic changes for uniformity- no physical changes

--- llc_1080/code/CPP_OPTIONS.h
+++ llc_1080/code/CPP_OPTIONS.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm_contrib/llc_hires/llc_1080/code/CPP_OPTIONS.h,v 1.1 2013/09/20 12:19:58 dimitri Exp $
+C $Header: /u/gcmpack/MITgcm_contrib/llc_hires/llc_1080/code/CPP_OPTIONS.h,v 1.2 2013/10/24 22:04:40 dimitri Exp $
 C $Name:  $
 
 #ifndef CPP_OPTIONS_H
@@ -32,7 +32,7 @@ C o Include/exclude call to S/R CALC_DIFFUSIVITY
 #define INCLUDE_CALC_DIFFUSIVITY_CALL
 
 C o Allow full 3D specification of vertical diffusivity
-#define ALLOW_3D_DIFFKR
+#undef ALLOW_3D_DIFFKR
 
 C o Allow latitudinally varying BryanLewis79 vertical diffusivity
 #undef ALLOW_BL79_LAT_VARY
