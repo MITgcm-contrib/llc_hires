d2731d477c660061ff8bf0bfe11137df3a4c826f
Hong Zhang <hong.zhang@jpl.nasa.gov>
2014-03-20 21:44:25 +0000

    accommodate STANDARD diagnostics; add profiles

--- llc_270/code/DIAGNOSTICS_SIZE.h
+++ llc_270/code/DIAGNOSTICS_SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm_contrib/llc_hires/llc_270/code/DIAGNOSTICS_SIZE.h,v 1.1 2014/03/06 01:09:23 dimitri Exp $
+C $Header: /u/gcmpack/MITgcm_contrib/llc_hires/llc_270/code/DIAGNOSTICS_SIZE.h,v 1.2 2014/03/20 21:44:25 zhc Exp $
 C $Name:  $
 
 
@@ -21,7 +21,7 @@ C  and "diagSt_size" (statistics-diags) since values here are deliberately small
       INTEGER    nRegions, sizRegMsk, nStats
       INTEGER    diagSt_size
       PARAMETER( ndiagMax = 700 )
-      PARAMETER( numlists = 70, numperlist = 2, numLevels=2*Nr )
+      PARAMETER( numlists = 70, numperlist = 30, numLevels=2*Nr )
       PARAMETER( numDiags = 900 )
       PARAMETER( nRegions = 0 , sizRegMsk = 1 , nStats = 4 )
       PARAMETER( diagSt_size = 10*Nr )
