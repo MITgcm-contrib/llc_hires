57f791feb273b38b9baf47f0d5fa7af5c8097751
hongandyan <hong.zhang@jpl.nasa.gov>
2023-10-27 17:20:53 -0700

    update GMREDI

--- llc_90/ecco_v4r5/code/GMREDI_OPTIONS.h
+++ llc_90/ecco_v4r5/code/GMREDI_OPTIONS.h
@@ -1,35 +1,36 @@
-C $Header: /u/gcmpack/MITgcm_contrib/ecco_utils/ecco_v4_release3_devel/code/GMREDI_OPTIONS.h,v 1.1 2017/05/04 17:46:37 ou.wang Exp $
-C $Name:  $
-
-C CPP options file for GM/Redi package
-C
-C Use this file for selecting options within the GM/Redi package
-
 #ifndef GMREDI_OPTIONS_H
 #define GMREDI_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
 #include "CPP_OPTIONS.h"
 
+CBOP
+C !ROUTINE: GMREDI_OPTIONS.h
+C !INTERFACE:
+C #include "GMREDI_OPTIONS.h"
+
+C !DESCRIPTION:
+C *==================================================================*
+C | CPP options file for GM/Redi package:
+C | Control which optional features to compile in this package code.
+C *==================================================================*
+CEOP
+
 #ifdef ALLOW_GMREDI
 C     Package-specific Options & Macros go here
 
-C initialize KapGM and KapRedi from a file
-chzh[
-#define ALLOW_KAPGM_CONTROL
-#define ALLOW_KAPREDI_CONTROL
-chzh]
-#define ALLOW_KAPGM_3DFILE
-#define ALLOW_KAPREDI_3DFILE
-
 C Designed to simplify the Ajoint code:
 #define GMREDI_WITH_STABLE_ADJOINT
-
 C -- exclude the clipping/tapering part of the code that is not used
 #define GM_EXCLUDE_CLIPPING
 #define GM_EXCLUDE_FM07_TAP
 #define GM_EXCLUDE_AC02_TAP
 C #define GM_EXCLUDE_TAPERING
 #define GM_EXCLUDE_SUBMESO
+
+C Allows to read-in background 3-D Redi and GM diffusivity coefficients
+C Note: need these to be defined for use as control (pkg/ctrl) parameters
+#define GM_READ_K3D_REDI
+#define GM_READ_K3D_GM
 
 C This allows to use Visbeck et al formulation to compute K_GM+Redi
 #undef GM_VISBECK_VARIABLE_K
@@ -39,8 +40,8 @@ C (which depends on tapering scheme)
 
 C This allows the Bates et al formulation to calculate the
 C bolus transport and K for Redi
-#undef GM_K3D
-#undef GM_K3D_PASSIVE
+#undef GM_BATES_K3D
+#undef GM_BATES_PASSIVE
 
 C This allows the leading diagonal (top two rows) to be non-unity
 C (a feature required when tapering adiabatically).
@@ -57,9 +58,12 @@ C  instead of the Skew-Flux form (=default)
 C Allows to use the Boundary-Value-Problem method to evaluate GM Bolus transport
 #undef GM_BOLUS_BVP
 
+C Allow QG Leith variable viscosity to be added to GMRedi coefficient
+#undef ALLOW_GM_LEITH_QG
+
+C Related to Adjoint-code:
+#undef GM_AUTODIFF_EXCESSIVE_STORE
+#undef GMREDI_MASK_SLOPES
+
 #endif /* ALLOW_GMREDI */
 #endif /* GMREDI_OPTIONS_H */
-
-CEH3 ;;; Local Variables: ***
-CEH3 ;;; mode:fortran ***
-CEH3 ;;; End: ***
