e5a16568f810e76d86fc1f9824c8391d26f4ad82
kayhan-momeni-1995 <94969213+kayhan-momeni-1995@users.noreply.github.com>
2025-11-14 23:16:49 -0500

    Update awk command to use printf for output
    Change print to printf for formatted output.

--- trillium/llc_4320/input/extract_blank.sh
+++ trillium/llc_4320/input/extract_blank.sh
@@ -4,8 +4,7 @@ infile="$1"
 outfile="blank"
 
 awk '
-  # Look for: Empty tile: # <spaces> <number>
   match($0, /Empty tile: #[[:space:]]*([0-9]+)/, m) {
-    print m[1]      # m[1] is the captured number = [N4]
+    printf "%s,\n", m[1]    # [N4],
   }
 ' "$infile" > "$outfile"
