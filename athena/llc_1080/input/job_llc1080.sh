2ea49dcd49254bac360a40557dfeb690b252db91
menemenlis <dimitri.menemenlis@sjsu.edu>
2026-01-21 13:28:30 -0800

    need 10 nodes (not 9)

--- athena/llc_1080/input/job_llc1080.sh
+++ athena/llc_1080/input/job_llc1080.sh
@@ -1,4 +1,4 @@
-#PBS -l select=9:ncpus=256:mpiprocs=256:model=tur_ath
+#PBS -l select=10:ncpus=256:mpiprocs=256:model=tur_ath
 #PBS -l walltime=2:00:00
 #PBS -l place=scatter:excl
 #PBS -q normal  
@@ -13,8 +13,15 @@ FI_PROVIDER=cxi
 
 WORKDIR=/nobackup/$USER/llc_1080
 
+cd $WORKDIR/MITgcm
+echo $PWD
+
+mv run run_old
+mv build build_old
+mkdir build run
 cd $WORKDIR/MITgcm/build
-rm -rf *
+echo $PWD
+
 cp ../../llc_hires/athena/llc_1080/code-async/SIZE.h_90x54x2229 SIZE.h
 ../tools/genmake2 -mpi -mods \
  '../../llc_hires/athena/llc_1080/code-async ../../llc_hires/athena/llc_1080/code' \
@@ -23,6 +30,8 @@ make depend
 make -j
 
 cd $WORKDIR/MITgcm/run
+echo $PWD
+
 cp ../build/mitgcmuv mitgcmuv_90x54x2229_asyncio
 ln -sf /nobackup/kzhang/llc1080/run_template/*1jan23* .
 ln -sf /nobackup/kzhang/llc1080/run_template/jra55* .
@@ -34,5 +43,5 @@ ln -sf /nobackup/dmenemen/forcing/SPICE/kernels .
 cp ../../llc_hires/athena/llc_1080/input/* .
 cp data_asyncio data
 cp data.exch2_90x54x2229 data.exch2
-FI_CXI_DEFAULT_TX_SIZE=65536
+#FI_CXI_DEFAULT_TX_SIZE=65536
 mpiexec -n 2560 ./mitgcmuv_90x54x2229_asyncio
