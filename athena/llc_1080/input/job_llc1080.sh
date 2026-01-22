e3cff887566314b1df4f7cfcee2ca2bf0978de5e
menemenlis <dimitri.menemenlis@sjsu.edu>
2026-01-21 18:09:44 -0800

    cosmetic change

--- athena/llc_1080/input/job_llc1080.sh
+++ athena/llc_1080/input/job_llc1080.sh
@@ -1,24 +1,25 @@
+#!/bin/bash
+
 #PBS -l select=10:ncpus=256:mpiprocs=256:model=tur_ath
 #PBS -l walltime=2:00:00
 #PBS -l place=scatter:excl
-#PBS -q normal  
+#PBS -q normal
 
-# If using ProEnv-intel instead of PrgEnv-cray, uncomment the line below
+# Switch to ProEnv-intel instead of PrgEnv-cray
 source /opt/cray/pe/modules/3.2.11.7/init/bash
 module swap PrgEnv-cray PrgEnv-intel
 
 #set FI_PROVIDER may reduce MPI startup time 
-#with bash
 FI_PROVIDER=cxi
 
 WORKDIR=/nobackup/$USER/llc_1080
-
 cd $WORKDIR/MITgcm
 echo $PWD
 
 mv run run_old
 mv build build_old
 mkdir build run
+
 cd $WORKDIR/MITgcm/build
 echo $PWD
 
@@ -33,6 +34,10 @@ cd $WORKDIR/MITgcm/run
 echo $PWD
 
 cp ../build/mitgcmuv mitgcmuv_90x54x2229_asyncio
+cp ../../llc_hires/athena/llc_1080/input/* .
+cp data_asyncio data
+cp data.exch2_90x54x2229 data.exch2
+
 ln -sf /nobackup/kzhang/llc1080/run_template/*1jan23* .
 ln -sf /nobackup/kzhang/llc1080/run_template/jra55* .
 ln -sf /nobackup/kzhang/llc1080/run_template/*_on_LLC1080_v13* .
@@ -40,8 +45,5 @@ ln -sf /nobackup/dmenemen/tarballs/llc_1080/run_template/tile00* .
 ln -sf /nobackup/dmenemen/tarballs/llc_4320/run_template/runoff1p2472-360x180x12.bin .
 ln -sf /nobackup/hzhang1/forcing/era5 .
 ln -sf /nobackup/dmenemen/forcing/SPICE/kernels .
-cp ../../llc_hires/athena/llc_1080/input/* .
-cp data_asyncio data
-cp data.exch2_90x54x2229 data.exch2
-#FI_CXI_DEFAULT_TX_SIZE=65536
+
 mpiexec -n 2560 ./mitgcmuv_90x54x2229_asyncio
