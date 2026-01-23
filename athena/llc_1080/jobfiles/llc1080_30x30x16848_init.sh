094e39a529f72b4d60815c8ccb4332bc9d935588
menemenlis <dimitris.menemenlis@sjsu.edu>
2026-01-23 08:09:57 -0800

    adding 30x30x16848 configuration

---
+++ athena/llc_1080/jobfiles/llc1080_30x30x16848_init.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#PBS -l select=66:ncpus=256:mpiprocs=256:model=tur_ath
+#PBS -l walltime=2:00:00
+#PBS -l place=scatter:excl
+#PBS -q normal
+
+# Switch to ProEnv-intel instead of PrgEnv-cray
+source /opt/cray/pe/modules/3.2.11.7/init/bash
+module swap PrgEnv-cray PrgEnv-intel
+
+#set FI_PROVIDER may reduce MPI startup time 
+FI_PROVIDER=cxi
+
+WORKDIR=/nobackup/$USER/llc_1080
+cd $WORKDIR/MITgcm
+echo $PWD
+
+mv run run_old
+mv build build_old
+mkdir build run
+
+cd $WORKDIR/MITgcm/build
+echo $PWD
+
+cp ../../llc_hires/athena/llc_1080/code/SIZE.h_30x30x16848 SIZE.h
+../tools/genmake2 -mpi -mods ../../llc_hires/athena/llc_1080/code \
+ -of ../../llc_hires/athena/llc_1080/code/linux_amd64_ifort+mpi_cray_nas_tides
+make depend
+make -j
+
+cd $WORKDIR/MITgcm/run
+echo $PWD
+
+cp ../build/mitgcmuv mitgcmuv_30x30x16848
+cp ../../llc_hires/athena/llc_1080/input/* .
+cp data_init data
+
+ln -sf /nobackup/kzhang/llc1080/run_template/*1jan23* .
+ln -sf /nobackup/kzhang/llc1080/run_template/jra55* .
+ln -sf /nobackup/kzhang/llc1080/run_template/*_on_LLC1080_v13* .
+ln -sf /nobackup/dmenemen/tarballs/llc_1080/run_template/tile00* .
+ln -sf /nobackup/dmenemen/tarballs/llc_4320/run_template/runoff1p2472-360x180x12.bin .
+ln -sf /nobackup/hzhang1/forcing/era5 .
+ln -sf /nobackup/dmenemen/forcing/SPICE/kernels .
+
+mpiexec -n 2560 ./mitgcmuv_90x54x2229_asyncio
--- athena/llc_1080/readme_athena.txt
+++ athena/llc_1080/readme_athena.txt
@@ -78,5 +78,5 @@ mpiexec -n 2229 ./mitgcmuv_90x54x2229 &
 cd $WORKDIR
 mkdir jobs
 cd $WORKDIR/jobs
-cp $WORKDIR/llc_hires/athena/llc_1080/input/job_llc1080.sh .
-qsub job_llc1080.sh
+cp $WORKDIR/llc_hires/athena/llc_1080/jobfiles/llc1080_90x54x2229_asyncio.sh .
+qsub llc1080_90x54x2229_asyncio.sh
