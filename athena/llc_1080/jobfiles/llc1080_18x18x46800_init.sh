fb20001b977c26521952d600ef4146a1a9905f1c
Kate Qian Zhang <kzhang@champlain.lan>
2026-01-28 05:38:35 -0800

    blan tile generation for 18x18x46800

---
+++ athena/llc_1080/jobfiles/llc1080_18x18x46800_init.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+
+#PBS -l select=128:ncpus=256:mpiprocs=366:model=tur_ath
+#PBS -l walltime=2:00:00
+#PBS -l place=scatter:excl
+#PBS -q wide
+
+# define tiling configuration
+RANKS=46800
+TILES=_18x18x$RANKS
+
+# Switch to ProEnv-intel instead of PrgEnv-cray
+source /opt/cray/pe/modules/3.2.11.7/init/bash
+module swap PrgEnv-cray PrgEnv-intel
+
+#set FI_PROVIDER may reduce MPI startup time 
+export FI_PROVIDER=cxi
+
+WORKDIR=/nobackup/$USER/llc_1080
+cd $WORKDIR/MITgcm
+
+mkdir build$TILES run$TILES
+
+cd $WORKDIR/MITgcm/build$TILES
+
+cp ../../llc_hires/athena/llc_1080/code/SIZE.h$TILES SIZE.h
+../tools/genmake2 -mpi -mods ../../llc_hires/athena/llc_1080/code \
+ -of ../../llc_hires/athena/llc_1080/code/linux_amd64_ifort+mpi_cray_nas_tides
+make depend
+make -j
+
+cd $WORKDIR/MITgcm/run$TILES
+
+cp ../build$TILES/mitgcmuv mitgcmuv$TILES
+cp ../../llc_hires/athena/llc_1080/input/* .
+cp data_init data
+
+ln -sf /nobackup/kzhang/llc1080/run_template/*1jan23* .
+ln -sf /nobackup/kzhang/llc1080/run_template/jra55* .
+ln -sf /nobackup/kzhang/llc1080/run_template/*_on_LLC1080_v13* .
+ln -sf /nobackup/dmenemen/tarballs/llc_1080/run_template/tile00* .
+ln -sf /nobackup/hzhang1/forcing/era5 .
+ln -sf /nobackup/dmenemen/forcing/SPICE/kernels .
+
+mpiexec -n $RANKS ./mitgcmuv$TILES
