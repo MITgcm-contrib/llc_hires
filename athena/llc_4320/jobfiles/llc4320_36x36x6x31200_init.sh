169d40b9cb8cc77d3c2f49732b08b110076e60a1
menemenlis <dimitris.menemenlis@sjsu.edu>
2026-01-29 14:56:02 -0800

    36x36x6x31200 blank tile computation

---
+++ athena/llc_4320/jobfiles/llc4320_36x36x6x31200_init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -x
+
+#PBS -l select=122:ncpus=256:mpiprocs=256:model=tur_ath
+#PBS -l walltime=2:00:00
+#PBS -l place=scatter:excl
+#PBS -q wide
+
+# define tiling configuration
+RANKS=31200
+TILES=_72x72x6x$RANKS
+
+# Switch to ProEnv-intel instead of PrgEnv-cray
+source /opt/cray/pe/modules/3.2.11.7/init/bash
+module swap PrgEnv-cray PrgEnv-intel
+
+#set FI_PROVIDER may reduce MPI startup time 
+export FI_PROVIDER=cxi
+
+WORKDIR=/nobackup/$USER/llc_4320
+cd $WORKDIR/MITgcm
+
+mkdir build$TILES run$TILES
+
+cd $WORKDIR/MITgcm/build$TILES
+
+cp ../../llc_hires/athena/llc_4320/code/SIZE.h$TILES SIZE.h
+../tools/genmake2 -mpi -mods ../../llc_hires/athena/llc_4320/code \
+ -of ../../llc_hires/athena/llc_4320/code/linux_amd64_ifort+mpi_cray_nas_tides
+make depend
+make -j
+
+cd $WORKDIR/MITgcm/run$TILES
+
+cp ../build$TILES/mitgcmuv mitgcmuv$TILES
+cp ../../llc_hires/athena/llc_4320/input/* .
+cp data_init data
+
+ln -sf /nobackup/kzhang/llc_4320/run_template/* .
+ln -sf /nobackup/kzhang/llc1080/run_template/jra55* .
+ln -sf /nobackup/dmenemen/tarballs/llc_4320/run_template/tile00* .
+ln -sf /nobackup/hzhang1/forcing/era5 .
+ln -sf /nobackup/dmenemen/forcing/SPICE/kernels .
+
+mpiexec -n $RANKS ./mitgcmuv$TILES
