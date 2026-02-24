9b82b0da443bd88097838bbdaf23a12993a11f5a
menemenlis <dimitris.menemenlis@sjsu.edu>
2026-02-23 17:36:40 -0800

    attempting to build a SAL job file --- not yet tested

--- athena/llc_4320/jobfiles/llc4320_90x90x19492_sal.sh
+++ athena/llc_4320/jobfiles/llc4320_90x90x19492_sal.sh
@@ -1,23 +1,39 @@
-#!/bin/bash
-#PBS -l select=9:ncpus=256:mpiprocs=256:model=tur_ath
-#PBS -l place=scatter:excl
-##PBS -q vlong
-##PBS -l walltime=42:00:00
-##PBS -q devel
-#PBS -q normal
-#PBS -l walltime=2:00:00
-#PBS -j oe
-#PBS -m abe
+#!/bin/bash -x
 
+#PBS -l select=128:ncpus=256:mpiprocs=160:model=tur_ath
+#PBS -l walltime=24:00:00
+#PBS -l place=scatter:excl
+#PBS -q wide
+#PBS -j oe
+
+# Define tiling configuration
+RANKS=19492
+TILES=_90x90x$RANKS
+
+# Switch to ProEnv-intel instead of PrgEnv-cray
 source /opt/cray/pe/modules/3.2.11.7/init/bash
 module swap PrgEnv-cray PrgEnv-intel
-module use /opt/cray/pals/modulefiles
-module load cray-pals
+module use /u/ojahn/software/modulefiles
+module load jahn/shtns/3.4.5_intel-2023.2.1
 
-#set FI_PROVIDER may reduce MPI startup time 
-FI_PROVIDER=cxi
+# Set FI_PROVIDER may reduce MPI startup time 
+export FI_PROVIDER=cxi
+export FI_CXI_RX_MATCH_MODE=hybrid
+export FI_CXI_DEFAULT_TX_SIZE=4096
 
-cd $PBS_O_WORKDIR
-ulimit -s unlimited
-mpiexec -np 2229 ./mitgcmuv
+WORKDIR=/nobackup/$USER/llc_4320
+cd $WORKDIR/MITgcm
+mkdir run$TILES
+cd $WORKDIR/MITgcm/run$TILES
+cp ../build$TILES/mitgcmuv mitgcmuv$TILES
+cp ../../llc_hires/athena/llc_4320/input/* .
+cp ../../llc_hires/athena/llc_4320/input_sal/* .
+cp data.exch2$TILES data.exch2
 
+ln -sf /nobackup/kzhang/llc_4320/run_template/* .
+ln -sf /nobackup/kzhang/llc1080/run_template/jra55* .
+ln -sf /nobackup/dmenemen/tarballs/llc_4320/run_template/tile00* .
+ln -sf /nobackup/hzhang1/forcing/era5 .
+ln -sf /nobackup/dmenemen/forcing/SPICE/kernels .
+
+mpiexec -n 20480 --cpu-bind none /u/scicon/tools/bin/mbind.x -cs ./mitgcmuv$TILES
