b1d7163ab2d83657f9b8ae99260cf89fbbc0c30b
Dimitris Menemenlis <dmenemenlis@gmail.com>
2026-03-02 11:38:00 -0800

    adding 36x36 sal jobfile

---
+++ athena/llc_4320/jobfiles/llc4320_36x36x116857_sal.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+
+#PBS -l select=1:ncpus=1:model=tur_ath+465:ncpus=256:mpiprocs=256:model=tur_ath
+#PBS -l walltime=24:00:00
+#PBS -l place=scatter:excl
+#PBS -q R91719
+#PBS -j oe
+
+# Define tiling configuration
+RANKS=116857
+TILES=_36x36x$RANKS
+
+# Switch to ProEnv-intel instead of PrgEnv-cray
+source /opt/cray/pe/modules/3.2.11.7/init/bash
+module swap PrgEnv-cray PrgEnv-intel
+
+# Set FI_PROVIDER may reduce MPI startup time 
+export FI_PROVIDER=cxi
+export FI_CXI_RX_MATCH_MODE=hybrid
+export FI_CXI_DEFAULT_TX_SIZE=4096
+
+WORKDIR=/nobackup/$USER/llc_4320
+cd $WORKDIR/MITgcm
+mkdir run$TILES
+cd $WORKDIR/MITgcm/run$TILES
+cp ../build$TILES/mitgcmuv mitgcmuv$TILES
+cp ../../llc_hires/athena/llc_4320/input/* .
+cp ../../llc_hires/athena/llc_4320/input_sal/* .
+cp data.exch2$TILES data.exch2
+
+ln -sf /nobackup/kzhang/llc_4320/run_template/* .
+ln -sf /nobackup/kzhang/llc1080/run_template/jra55* .
+ln -sf /nobackup/dmenemen/tarballs/llc_4320/run_template/tile00* .
+ln -sf /nobackup/hzhang1/forcing/era5 .
+ln -sf /nobackup/dmenemen/forcing/SPICE/kernels .
+
+ulimit -s unlimited
+mpiexec -n 119041 ./mitgcmuv$TILES
