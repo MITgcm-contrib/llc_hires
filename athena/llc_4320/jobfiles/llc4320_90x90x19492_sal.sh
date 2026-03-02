9fece5e7f206f8e3ef1ce2df9f2a7414ce459091
Dimitris Menemenlis <dmenemenlis@gmail.com>
2026-03-02 12:04:05 -0800

    need Load_Love2_CM.dat file from llc_hires/athena/llc_1080/input_sal

--- athena/llc_4320/jobfiles/llc4320_90x90x19492_sal.sh
+++ athena/llc_4320/jobfiles/llc4320_90x90x19492_sal.sh
@@ -26,6 +26,7 @@ cd $WORKDIR/MITgcm/run$TILES
 cp ../build$TILES/mitgcmuv mitgcmuv$TILES
 cp ../../llc_hires/athena/llc_4320/input/* .
 cp ../../llc_hires/athena/llc_4320/input_sal/* .
+cp ../../llc_hires/athena/llc_1080/input_sal/Load_Love2_CM.dat .
 cp data.exch2$TILES data.exch2
 
 ln -sf /nobackup/kzhang/llc_4320/run_template/* .
